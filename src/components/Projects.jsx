becbaeda51269426d788e5b6a44b673d258a8d75
Daniel Xavier <100311104+DanielXavierJob@users.noreply.github.com>
2023-09-07 13:26:55 -0300

    ✨ feat: desc

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,8 +14,8 @@ export const Projects = () => {
       <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
         <h2 className={styles.heading2}>My projects</h2>
         <div className="w-full md:mt-0 mt-6">
-          <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-            The battle only becomes impossible when you stop fighting it.
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>    
+                I currently have +30 projects, so I leave some of them below for viewing
           </p>
         </div>
       </div>
